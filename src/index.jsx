23dce62a80e975bf1b47b4198f78d41afabca690
Donkor T. Jonas <59501716+kwqme-Jonas@users.noreply.github.com>
2023-01-08 12:09:40 +0000

    installed packages

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,10 @@
 import React from 'react';
-import  ReactDOM  from 'react';
+import ReactDOM from 'react';
+
+import App from './App';
+
+const root = ReactDOM.creatRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+);
